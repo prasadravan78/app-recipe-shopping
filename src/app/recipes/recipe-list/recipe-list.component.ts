8638700cecc4408d02e451833d6c39d2b646c09f
Prasad Ravan <ravan.prasad@mcsustainabilitydev.onmicrosoft.com>
2020-06-01 12:47:01 +0530

    Initial commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [new Recipe(
+      "Test Recipe",
+      "This is testing",
+      "https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/3:2/w_2560,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg"
+  )];
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared-component/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Chicken', 1),
+    new Ingredient('Onion', 2)
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

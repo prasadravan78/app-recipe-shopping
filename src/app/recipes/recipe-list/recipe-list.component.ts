a707355feef9d9ff6793cdd71498f7ef19c9b670
Prasad Ravan <prasadcravan@gmail.com>
2020-06-01 18:59:12 +0530

    Implemented on selection list item to show details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from "../recipe.model";
 
 @Component({
@@ -7,15 +7,21 @@ import { Recipe } from "../recipe.model";
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeToShow = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [new Recipe(
-      "Test Recipe",
-      "This is testing",
-      "https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/3:2/w_2560,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg"
+    "Test Recipe",
+    "This is testing",
+    "https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/3:2/w_2560,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg"
   )];
-  
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
+  onSelection(recipe: Recipe) {
+    console.log(recipe);
+    this.recipeToShow.emit(recipe);
+  }
 }
